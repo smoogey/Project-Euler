a928c46bdb89a53aa424ba2375ad899ca070da84
pledet <pledet@progeny.net>
2018-10-19 14:15:07 -0400

    Rewrote the core adjacent product algorithm to the way I would do it.

--- py8-phill.py
+++ py8-phill.py
@@ -1,19 +1,31 @@
+import time
+
 # Finds the larget product of 13 adjacent digits in this 1000 digit number.
 
 file_name = "input_files/adjSumInput.txt"
 
+def prod(list_to_prod):
+    prod = 1
+    for i in range(0, len(list_to_prod)):
+        prod = prod * list_to_prod[i]
+    return prod
+
 def adjac(num_of_ints, big_num):
-    productList = []
-    lengthOfBigNum = len(big_num)
-    factors = num_of_ints
-    position = 0
-    while position <= lengthOfBigNum - factors:
-        dummy = 1
-        for i in big_num[position : position + factors]:
-            dummy = dummy * int(i)
-            productList.append(dummy)
-        position += 1
-    return productList
+    product = 0
+    productIndex = 0
+    print(num_of_ints.__class__)
+    for i in range(0, len(big_num)-num_of_ints):
+        productCandidate = prod(big_num[i:i+num_of_ints])
+        if productCandidate > product:
+            product = productCandidate
+            productIndex = i
+
+    product = 1
+    for i in big_num[productIndex: productIndex + (num_of_ints)]:
+        product = product * i
+
+    return product
+
 
 digitList = []
 
@@ -23,6 +35,9 @@ with open(file_name) as f:
         if not c:
             break
         else:
-            digitList.append(c)
+            digitList.append(int(c))
 
-print(max(adjac(13, digitList)))
+startTime = time.time()* 1000
+print(adjac(13, digitList))
+print(str(time.time() * 1000 - startTime))
+
